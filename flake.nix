2f0ff9f9cfa9342ebd5bf7265f6bb790fca1a936
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2022-11-28 20:17:53 +0000

    Switch to the dune build system
    Add wasp/dune-project and wasp/dune files, based on the dune conversion
    from the spec interpreter[1]
    Change README to use an opam switch called wasp instead of just 4.08.1
    and to mention dune and how to use dune
    When compiling there were deprecation warnings for functions in
    String and Pervasives, they were modified to use the new "paths"
    - float.ml
    Some functions seemed to be from the batteries module or were in files
    that used to open the batteries modules, I changed them to use the full
    path
    - symbolic/Z3Encoding2.ml
    - symbolic/ranges.ml
    - symbolic/symvalue.ml
    I removed the file Z3Encoding and symmem.ml(i) since they were superseded
    
    [1](https://github.com/WebAssembly/spec/commit/6c2b582c6415370b310625eba114e08578a8c93f)

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
             wabt
           ];
           shellHook = ''
-          export LD_LIBRARY_PATH="$HOME/.opam/4.08.1/lib/z3/:$LD_LIBRARY_PATH"
+          export LD_LIBRARY_PATH="$HOME/.opam/wasp/lib/z3/:$LD_LIBRARY_PATH"
           export PATH="$PATH:$PWD/wasp"
           # export PATH="$PATH:$PWD/wasp-c/bin"
           eval $(opam env)
